6910d6fc8336f08ce3781eac14294a8fde89dfe5
Anuj Sharma <69591862+ProdexOne@users.noreply.github.com>
2021-06-11 09:25:51 +0530

    web preview function added

--- src/components/webRender/WebRender.tsx
+++ src/components/webRender/WebRender.tsx
@@ -1,15 +1,30 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "../context";
 import styles from "./WebRender.module.css";
 
 const WebRender = () => {
-  const [src] = useState("<h3>Web Render</h3>");
+  const { code } = useContext(AppContext);
+  const [srcDoc, setSrcDoc] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${code["index.html"]}</body>
+          <style>${code["style.css"]}</style>
+          <script>${code["app.js"]}</script>
+        </html>
+      `);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [code]);
 
   return (
     <div className={styles.container}>
       <div className={styles.web__bar}>Preview</div>
       <div className={styles.web__wrapper}>
         <iframe
-          srcDoc={src}
+          srcDoc={srcDoc}
           className={styles.iframe}
           title="webApp"
           src=""
