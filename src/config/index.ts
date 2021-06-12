1e31a3ad9558bed5c6d736cf7c9d9cb779c82ce6
Anuj Sharma <69591862+ProdexOne@users.noreply.github.com>
2021-06-12 19:15:09 +0530

    const update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const env = {
-  SERVER_URL: "http://localhost:80",
-  SOCKET_URL: "http://localhost:80",
+  SERVER_URL: "http://localhost:5500",
+  SOCKET_URL: "http://localhost:5500",
 };
 
 export const { SERVER_URL, SOCKET_URL } = env;
